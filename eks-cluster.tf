be6c8c244ac47ddcc71a4569299a517feb764730
William Haw <me@williamhaw.com>
2020-04-22 18:59:16 +0800

    Add K8s provider

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -36,3 +36,11 @@ data "aws_eks_cluster" "cluster" {
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.9"
+}
