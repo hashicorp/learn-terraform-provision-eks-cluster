aea9785472c04ef45a10576333d62a0bfd753c3f
Daniel <vioseven@gmail.com>
2020-10-10 20:04:24 +0200

    Fixing typo on kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,6 +1,6 @@
 # # Kubernetes provider
 # # https://learn.hashicorp.com/terraform/kubernetes/provision-eks-cluster#optional-configure-terraform-kubernetes-provider
-# # To learn how to schedule deployments and services using the provider, go here: ttps://learn.hashicorp.com/terraform/kubernetes/deploy-nginx-kubernetes.
+# # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/terraform/kubernetes/deploy-nginx-kubernetes
 
 provider "kubernetes" {
   load_config_file       = "false"
