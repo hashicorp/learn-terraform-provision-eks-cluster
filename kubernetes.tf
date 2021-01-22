fae49512e673e49547a01f8400790c9a5760b240
Marc Streeter <mstreeter@gmail.com>
2021-01-22 09:48:39 -0600

    updated for kubernetes provider version 2

--- kubernetes.tf
+++ kubernetes.tf
@@ -6,7 +6,6 @@
 # You should **not** schedule deployments and services in this workspace. This keeps workspaces modular (one for provision EKS, another for scheduling Kubernetes resources) as per best practices.
 
 provider "kubernetes" {
-  load_config_file       = "false"
   host                   = data.aws_eks_cluster.cluster.endpoint
   token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
--- versions.tf
+++ versions.tf
@@ -15,6 +15,11 @@ terraform {
       version = "2.0.0"
     }
 
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.1"
+    }
+
     null = {
       source  = "hashicorp/null"
       version = "3.0.0"
