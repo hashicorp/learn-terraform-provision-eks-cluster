f427a7dab0e46c43492a590ae45e24a033da3dbd
Ivanna Terletska <i.terletska1@gmail.com>
2021-02-10 17:44:49 +0200

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -8,6 +8,7 @@
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
     command     = "aws"
