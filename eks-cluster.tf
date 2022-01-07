38b9ba8f4483dff317d9af10bba397521c53b8a8
Yevgeny Kungurov <ekungurov83@gmail.com>
2022-01-07 15:28:37 +0400

    Fix it to work after terraform-aws-modules/eks/aws 18 release

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,5 +1,6 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
+  version         = "17.24.0"
   cluster_name    = local.cluster_name
   cluster_version = "1.20"
   subnets         = module.vpc.private_subnets
