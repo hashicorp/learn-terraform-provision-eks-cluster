4adfdf289291f39ddf41def15b84eba9e597061b
DevOps Arthur <alex@bnetzone.ro>
2022-09-22 15:33:56 +0300

    Added the S3 bucket terraform_state

--- outputs.tf
+++ outputs.tf
@@ -22,3 +22,10 @@ output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = local.cluster_name
 }
+output "s3_bucket_name" {
+  value = aws_s3_bucket.terraform_state.id
+}
+
+output "dynamodb_endpoint" {
+  value = aws_dynamodb_table.terraform_state_lock.name
+}
