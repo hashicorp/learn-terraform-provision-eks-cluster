7c9242210bbf3b87f9e4c70af7a3daa5a3a77d62
DevOps Arthur <alex@bnetzone.ro>
2022-09-22 15:38:10 +0300

    Added the S3 bucket terraform_state

--- main.tf
+++ main.tf
@@ -26,11 +26,11 @@ resource "aws_s3_bucket" "terraform_state" {
   bucket_prefix = "learn-s3-remote-backend-"
 }
 resource "aws_s3_bucket_acl" "terraform_state_acl" {
-  bucket = aws_s3_bucket.terraform_state
+  bucket = aws_s3_bucket.terraform_state.id
   acl    = "private"
 }
 resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
-  bucket = aws_s3_bucket.terraform_state
+  bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
     status = "Enabled"
   }
