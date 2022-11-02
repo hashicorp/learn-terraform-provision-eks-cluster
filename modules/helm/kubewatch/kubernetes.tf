a83976538e4bae98e82fd6f0e7e9f24ad1df228c
vignesh <r.vignesh88@gmail.com>
2022-11-02 17:44:25 +0530

    fix 12

--- modules/helm/kubewatch/kubernetes.tf
+++ modules/helm/kubewatch/kubernetes.tf
@@ -41,7 +41,7 @@ data "aws_eks_cluster_auth" "cluster" {
 }*/
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = output.cluster_id
 }
 
 resource "kubernetes_namespace" "terramino" {
