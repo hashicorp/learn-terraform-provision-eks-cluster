1d13ae4729880d703cbe3ac25434927efc66a235
vignesh <r.vignesh88@gmail.com>
2022-11-02 13:56:41 +0530

    fix 5

--- variables.tf
+++ variables.tf
@@ -3,3 +3,13 @@ variable "region" {
   type        = string
   default     = "us-east-2"
 }
+
+variable "application_name" {
+  type    = string
+  default = "terramino"
+}
+
+variable "slack_app_token" {
+  type        = string
+  description = "Slack App Token"
+}
