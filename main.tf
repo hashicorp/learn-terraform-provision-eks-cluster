ad9220e7b27d1fd28dca69df68a06b3b03ec4afd
vignesh <r.vignesh88@gmail.com>
2022-11-02 14:03:25 +0530

    fix 7

--- main.tf
+++ main.tf
@@ -26,6 +26,6 @@ resource "random_string" "suffix" {
 }
 
 module "kubewatch" {
-  source = "./modules/helm/kubewatch"
+  source          = "./modules/helm/kubewatch"
   slack_app_token = var.slack_app_token
 }
