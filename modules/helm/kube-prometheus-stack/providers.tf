7c220997e4e9e939a1b372d1fd6f000eda18b2f3
vignesh <r.vignesh88@gmail.com>
2022-11-02 23:04:21 +0530

    kube-prometheus-stack fix1

---
+++ modules/helm/kube-prometheus-stack/providers.tf
@@ -0,0 +1,48 @@
+/*data "terraform_remote_state" "eks" {
+  backend = "local"
+  config = {
+    path = "../learn-terraform-provision-eks-cluster/terraform.tfstate"
+  }
+}*/
+
+# Terraform remote state for backend remote/cloud
+data "terraform_remote_state" "eks" {
+  backend = "remote"
+  config = {
+    organization = "vigneshragupathy"
+
+    workspaces = {
+      name = "learn-terraform-provision-eks-cluster"
+    }
+  }
+}
+
+# Retrieve EKS cluster configuration
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_id
+}
+
+/*provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+    command     = "aws"
+  }
+}*/
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
+  }
+}
