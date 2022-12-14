803ab92b7a118233c51f394ade83926ad75ec904
Brian McClain <brianmmcclain@gmail.com>
2022-12-14 11:26:41 -0500

    Upgrade EKS module version, explicitly make API endpoint publicly available

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,12 +1,13 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "18.26.6"
+  version = "19.0.4"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.22"
+  cluster_version = "1.23"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
