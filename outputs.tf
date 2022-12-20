f39ec5f76b65aa8dffee5b92e0b6029d30c2ada9
Brian McClain <brianmmcclain@gmail.com>
2022-12-20 16:05:24 -0500

    cluster_id no longer provided in EKS module v19

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "cluster_id" {
-  description = "EKS cluster ID"
-  value       = module.eks.cluster_id
-}
-
 output "cluster_endpoint" {
   description = "Endpoint for EKS control plane"
   value       = module.eks.cluster_endpoint
