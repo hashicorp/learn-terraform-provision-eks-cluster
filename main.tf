ca1a80eec0ccdabf810840d561eaccd2245e4a72
Brian McClain <brianmmcclain@gmail.com>
2023-08-18 14:49:26 -0400

    Filter out local zones

--- main.tf
+++ main.tf
@@ -5,7 +5,14 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_availability_zones" "available" {}
+# Filter out local zones, which are not currently supported 
+# with managed node groups
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
 
 locals {
   cluster_name = "education-eks-${random_string.suffix.result}"
