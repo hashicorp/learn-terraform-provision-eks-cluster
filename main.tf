b606d3bf8e5f20de273ff02670496900bedda7dd
Brian McClain <brianmmcclain@gmail.com>
2024-02-16 13:48:03 -0500

    Bump provider, module, cluster version. Use cluster_addons pa rameter of EKS module

--- main.tf
+++ main.tf
@@ -52,14 +52,26 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.3"
+  version = "20.2.1"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.27"
+  cluster_version = "1.29"
+
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    aws-ebs-csi-driver = {
+      addon_version            = "v1.20.0-eksbuild.1"
+      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+      tags = {
+        "eks_addon" = "ebs-csi"
+        "terraform" = "true"
+      }
+    }
+  }
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
@@ -104,15 +116,4 @@ module "irsa-ebs-csi" {
   provider_url                  = module.eks.oidc_provider
   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
-
-resource "aws_eks_addon" "ebs-csi" {
-  cluster_name             = module.eks.cluster_name
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.20.0-eksbuild.1"
-  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-  tags = {
-    "eks_addon" = "ebs-csi"
-    "terraform" = "true"
-  }
 }
